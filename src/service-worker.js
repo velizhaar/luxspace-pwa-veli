b8f41bb88c415d151473628d1d3865fb5b35f7a3
velizhaar <velizhaar@gmail.com>
2021-04-19 23:32:07 +0700

    cache image dari REST API

--- src/service-worker.js
+++ src/service-worker.js
@@ -77,6 +77,31 @@ registerRoute(
   })
 )
 
+// agar API tetap bisa di akses ketika offline
+registerRoute(
+  ({ url }) => url.origin.includes("qorebase.io"),
+  new NetworkFirst({
+    cacheName: "apidata",
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 360,
+        maxEntries: 30,
+      }),
+    ],
+  })
+)
+registerRoute(
+  ({ url }) => /\.(jpe?g|png)$/i.test(url.pathname),
+  new StaleWhileRevalidate({
+    cacheName: "apiimage",
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 30,
+      }),
+    ],
+  })
+)
+
 self.addEventListener("install", function (event) {
   console.log("SW Install")
   const asyncInstall = new Promise(function (resolve) {
