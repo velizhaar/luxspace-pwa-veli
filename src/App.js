c95601c478a49effe8f70012c5d8f7c4740ffb71
velizhaar <velizhaar@gmail.com>
2021-05-01 00:42:52 +0700

    offline info dan manifest

--- src/App.js
+++ src/App.js
@@ -6,28 +6,51 @@ import Arrived from "./components/Arrived"
 import Clients from "./components/Clients"
 import AsideMenu from "./components/AsideMenu"
 import Footer from "./components/Footer"
+import Offline from "./components/Offline"
 
 function App() {
   const [items, setItems] = React.useState([])
+  const [offlineStatus, setOfflineStatus] = React.useState(!navigator.onLine)
 
-  React.useEffect(function () {
-    ;(async function () {
-      const response = await fetch(
-        "https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc",
-        {
-          headers: {
-            "Content-Type": "application/json",
-            accept: "application/json",
-            "x-api-key": process.env.REACT_APP_APIKEY,
-          },
-        }
-      )
-      const { nodes } = await response.json()
-      setItems(nodes)
-    })()
-  }, [])
+  function handleOfflineStatus() {
+    setOfflineStatus(!navigator.onLine)
+  }
+
+  React.useEffect(
+    function () {
+      ;(async function () {
+        const response = await fetch(
+          "https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              accept: "application/json",
+              "x-api-key": process.env.REACT_APP_APIKEY,
+            },
+          }
+        )
+        const { nodes } = await response.json()
+        setItems(nodes)
+
+        const script = document.createElement("script")
+        script.src = "/carousel.js"
+        script.async = false
+        document.body.appendChild(script)
+      })()
+      handleOfflineStatus()
+      window.addEventListener("online", handleOfflineStatus)
+      window.addEventListener("offline", handleOfflineStatus)
+
+      return function () {
+        window.removeEventListener("online", handleOfflineStatus)
+        window.removeEventListener("offline", handleOfflineStatus)
+      }
+    },
+    [offlineStatus]
+  )
   return (
     <>
+      {offlineStatus && <Offline />}
       <Header />
       <Hero />
       <Browse />
